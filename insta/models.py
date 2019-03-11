03746e469a206c194384b824cda13e936f14f893
blackrose <wahomekelving@gmail.com>
2019-03-11 10:26:43 +0300

    Created profile model

--- insta/models.py
+++ insta/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+class Profile(models.Model):
+  pic = models.ImageField(upload_to='images/',blank=True)
+  user = models.OneToOneField(User,null = True,on_delete=models.CASCADE,related_name = "profile")
+  bio = models.TextField()
+
 class Image(models.Model):
   name = models.CharField(max_length=50)
   image = models.ImageField(upload_to = 'images/',blank=True)
