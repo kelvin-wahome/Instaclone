1c1b1c43e5d853407f444045403c06d38628e769
blackrose <wahomekelving@gmail.com>
2019-03-12 12:47:59 +0300

    Added search user view function

--- insta/views.py
+++ insta/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render,redirect
 from .models import Image,Profile,Comment,Likes
+from django.http import HttpResponse
+
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from friendship.models import Friend, Follow, Block
+from .forms import SignupForm,ImageForm,CommentForm,ProfileForm
+
 
 
 @login_required(login_url='/accounts/login/')
@@ -14,3 +18,21 @@ def index(request):
     people = Follow.objects.following(request.user)
     profile = User.objects.all()
     return render(request,'index.html',locals())
+
+@login_required(login_url='/accounts/login/')
+def search_user(request):
+    """
+    Function that searches for profiles based on the usernames
+    """
+    if 'username' in request.GET and request.GET["username"]:
+        name = request.GET.get("username")
+        searched_profiles = User.objects.filter(username__icontains=name)
+        message = f"{name}"
+        profiles = User.objects.all()
+        people = Follow.objects.following(request.user)
+        print(profiles)
+        return render(request, 'search.html', {"message":message, "usernames":searched_profiles, "profiles":profiles,})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {"message":message})
