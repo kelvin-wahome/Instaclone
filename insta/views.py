fe94a2cd026719f0f999fcc3d69ef61a169ab26e
blackrose <wahomekelving@gmail.com>
2019-03-12 14:52:48 +0300

    Added view function to add a new image

--- insta/views.py
+++ insta/views.py
@@ -19,6 +19,22 @@ def index(request):
     profile = User.objects.all()
     return render(request,'index.html',locals())
 
+@login_required(login_url='accounts/login/')
+def add_new_image(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            add=form.save(commit=False)
+            add.profile = current_user
+            add.save()
+            return redirect('indexpage')
+    else:
+        form = ImageForm()
+
+
+    return render(request,'post.html',locals())
+
 @login_required(login_url='/accounts/login/')
 def search_user(request):
     """
