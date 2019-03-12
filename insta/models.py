7ac58917722a652eeea01c1ccafeb70aeb3346af
blackrose <wahomekelving@gmail.com>
2019-03-12 13:01:32 +0300

    Added search profile class methods

--- insta/models.py
+++ insta/models.py
@@ -7,30 +7,43 @@ class Profile(models.Model):
   bio = models.TextField()
 
 
-  @receiver(post_save, sender=User)
-  def create_user_profile(sender, instance, created, **kwargs):
-        if created:
-                Profile.objects.create(user=instance)
-  @receiver(post_save, sender=User)
-  def save_user_profile(sender, instance, **kwargs):
-            instance.profile.save()
+      @receiver(post_save, sender=User)
+      def create_user_profile(sender, instance, created, **kwargs):
+            if created:
+                    Profile.objects.create(user=instance)
+      @receiver(post_save, sender=User)
+      def save_user_profile(sender, instance, **kwargs):
+                instance.profile.save()
 
-  post_save.connect(save_user_profile, sender=User)
-
-
-  def __str__(self):
-    return self.bio
-
-  def save_profile(self):
-    self.save()
-
-  def update_profile(self):
-    self.update()
-
-  def delete_profile(self):
-    self.delete()
+      post_save.connect(save_user_profile, sender=User)
 
 
+      def __str__(self):
+        return self.bio
+
+      def save_profile(self):
+        self.save()
+
+      def update_profile(self):
+        self.update()
+
+      def delete_profile(self):
+        self.delete()
+
+      @classmethod
+          def search_profile(cls, name):
+            profile = Profile.objects.filter(user__username__icontains = name)
+            return profile
+
+      @classmethod
+      def get_profile_by_id(cls, id):
+          user_profile = Profile.objects.get(user=id)
+          return user_profile
+
+      @classmethod
+      def get_profile_by_username(cls, user):
+          profile_info = cls.objects.filter(user__contains=user)
+          return profile_info
 
 class Image(models.Model):
   name = models.CharField(max_length=50)
